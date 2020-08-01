f1f6a6d5920b083e03be7277d9f150af9007ebd9
DeekshaMax <59228328+DeekshaMax@users.noreply.github.com>
2020-08-01 16:43:19 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+"use strict";
+function string_to_array()
+{
+  var str=document.getElementById("stringtext").value;
+  var res = str.split(" ")
+  document.getElementById("arrayofwords").innerHTML=res;
+}
